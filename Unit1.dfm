8f2e7799cfcdbe5fe81514ea7027a3770f2fd137
enyniswa <enyniswatuhasanah@gmail.com>
2024-07-12 22:17:48 +0800

    UAS_VISUAL2

---
+++ Unit1.dfm
@@ -0,0 +1,219 @@
+object Form1: TForm1
+  Left = 192
+  Top = 125
+  Width = 928
+  Height = 480
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnShow = FormShow
+  PixelsPerInch = 96
+  TextHeight = 13
+  object lbl1: TLabel
+    Left = 256
+    Top = 40
+    Width = 17
+    Height = 13
+    Caption = 'NIK'
+  end
+  object lbl2: TLabel
+    Left = 256
+    Top = 64
+    Width = 29
+    Height = 13
+    Caption = 'NAMA'
+  end
+  object lbl3: TLabel
+    Left = 256
+    Top = 88
+    Width = 23
+    Height = 13
+    Caption = 'TELP'
+  end
+  object lbl4: TLabel
+    Left = 256
+    Top = 112
+    Width = 30
+    Height = 13
+    Caption = 'EMAIL'
+  end
+  object lbl5: TLabel
+    Left = 256
+    Top = 136
+    Width = 40
+    Height = 13
+    Caption = 'ALAMAT'
+  end
+  object lbl6: TLabel
+    Left = 256
+    Top = 160
+    Width = 41
+    Height = 13
+    Caption = 'MEMBER'
+  end
+  object lbl7: TLabel
+    Left = 408
+    Top = 168
+    Width = 60
+    Height = 13
+    Caption = ' DISKON     :'
+  end
+  object lbl8: TLabel
+    Left = 480
+    Top = 168
+    Width = 71
+    Height = 13
+    Caption = 'Terisi Otomatis'
+  end
+  object lbl9: TLabel
+    Left = 208
+    Top = 384
+    Width = 77
+    Height = 13
+    Caption = 'Masukkan Nama'
+  end
+  object edt1: TEdit
+    Left = 312
+    Top = 40
+    Width = 161
+    Height = 21
+    TabOrder = 0
+  end
+  object edt2: TEdit
+    Left = 312
+    Top = 64
+    Width = 161
+    Height = 21
+    TabOrder = 1
+  end
+  object edt3: TEdit
+    Left = 312
+    Top = 88
+    Width = 161
+    Height = 21
+    TabOrder = 2
+  end
+  object edt4: TEdit
+    Left = 312
+    Top = 112
+    Width = 161
+    Height = 21
+    TabOrder = 3
+  end
+  object edt5: TEdit
+    Left = 312
+    Top = 136
+    Width = 161
+    Height = 21
+    TabOrder = 4
+  end
+  object cbb1: TComboBox
+    Left = 312
+    Top = 160
+    Width = 89
+    Height = 21
+    ItemHeight = 13
+    TabOrder = 5
+    OnChange = cbb1Change
+    Items.Strings = (
+      'YES'
+      'NO')
+  end
+  object btn1: TButton
+    Left = 168
+    Top = 200
+    Width = 81
+    Height = 33
+    Caption = 'BARU'
+    TabOrder = 6
+  end
+  object btn2: TButton
+    Left = 264
+    Top = 200
+    Width = 81
+    Height = 33
+    Caption = 'SIMPAN'
+    TabOrder = 7
+    OnClick = btn2Click
+  end
+  object btn3: TButton
+    Left = 360
+    Top = 200
+    Width = 81
+    Height = 33
+    Caption = 'EDIT'
+    TabOrder = 8
+    OnClick = btn3Click
+  end
+  object btn4: TButton
+    Left = 456
+    Top = 200
+    Width = 81
+    Height = 33
+    Caption = 'HAPUS'
+    TabOrder = 9
+  end
+  object btn5: TButton
+    Left = 552
+    Top = 200
+    Width = 81
+    Height = 33
+    Caption = 'BATAL'
+    TabOrder = 10
+  end
+  object dbgrd1: TDBGrid
+    Left = 232
+    Top = 256
+    Width = 393
+    Height = 113
+    DataSource = ds1
+    TabOrder = 11
+    TitleFont.Charset = DEFAULT_CHARSET
+    TitleFont.Color = clWindowText
+    TitleFont.Height = -11
+    TitleFont.Name = 'Tahoma'
+    TitleFont.Style = []
+  end
+  object edt6: TEdit
+    Left = 296
+    Top = 376
+    Width = 161
+    Height = 21
+    TabOrder = 12
+    Text = 'edt1'
+  end
+  object con1: TZConnection
+    ControlsCodePage = cGET_ACP
+    Properties.Strings = (
+      'RawStringEncoding=DB_CP')
+    Connected = True
+    DisableSavepoints = False
+    HostName = 'localhost'
+    Port = 3306
+    Database = 'penjualan_uasvisual2'
+    User = 'root'
+    Protocol = 'mysql'
+    LibraryLocation = 'E:\Semester 4\Visual 2\fiture_member\libmysql.dll'
+    Left = 16
+    Top = 8
+  end
+  object zqry1: TZQuery
+    Connection = con1
+    Active = True
+    SQL.Strings = (
+      'select * from kustomer')
+    Params = <>
+    Left = 72
+    Top = 8
+  end
+  object ds1: TDataSource
+    DataSet = zqry1
+    Left = 128
+    Top = 16
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,180 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Grids, DBGrids, StdCtrls, DB, ZAbstractRODataset,
+  ZAbstractDataset, ZDataset, ZAbstractConnection, ZConnection;
+
+type
+  TForm1 = class(TForm)
+    lbl1: TLabel;
+    lbl2: TLabel;
+    lbl3: TLabel;
+    lbl4: TLabel;
+    lbl5: TLabel;
+    lbl6: TLabel;
+    lbl7: TLabel;
+    lbl8: TLabel;
+    edt1: TEdit;
+    edt2: TEdit;
+    edt3: TEdit;
+    edt4: TEdit;
+    edt5: TEdit;
+    cbb1: TComboBox;
+    btn1: TButton;
+    btn2: TButton;
+    btn3: TButton;
+    btn4: TButton;
+    btn5: TButton;
+    dbgrd1: TDBGrid;
+    lbl9: TLabel;
+    edt6: TEdit;
+    con1: TZConnection;
+    zqry1: TZQuery;
+    ds1: TDataSource;
+    procedure posisiawal;
+    procedure bersih;
+    procedure btn2Click(Sender: TObject);
+    procedure cbb1Change(Sender: TObject);
+    procedure btn3Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  a: string;
+
+implementation
+
+{$R *.dfm}
+
+{ TForm1 }
+
+procedure TForm1.bersih;
+begin
+edt1.Clear;
+edt2.Clear;
+edt3.Clear;
+edt4.Clear;
+edt5.Clear;
+cbb1.Clear;
+end;
+
+procedure TForm1.posisiawal;
+begin
+btn1.Enabled:= True;
+btn2.Enabled:= False;
+btn3.Enabled:= False;
+btn4.Enabled:= False;
+btn5.Enabled:= False;
+edt1.Clear;
+edt2.Clear;
+edt3.Clear;
+edt4.Clear;
+edt5.Clear;
+cbb1.Clear;
+edt1.Enabled:=False;
+edt2.Enabled:=False;
+edt3.Enabled:=False;
+edt4.Enabled:=False;
+edt5.Enabled:=False;
+cbb1.Enabled:=False;
+end;
+
+procedure TForm1.btn2Click(Sender: TObject);
+//code untuk button simpan
+begin
+if zqry1.Locate('nama',edt2.Text,[])then
+begin
+  ShowMessage('Nama'+ edt2.Text+' Sudah Ada Didalam Sistem');
+end else
+begin
+  with zqry1 do
+  begin
+    SQL.Clear;
+    SQL.Add('Insert into kustomer values(null, "' + edt1.Text + '", "' + edt2.Text + '", "' + edt3.Text + '", "' + edt4.Text + '", "' + edt5.Text + '", "' + cbb1.Text + '")');
+    ExecSQL;
+    SQL.Clear;
+    SQL.Add('Select * from kustomer');
+    Open;
+  end;
+  ShowMessage('Data Berhasil Disimpan!');
+end;
+  posisiawal;
+end;
+
+procedure TForm1.cbb1Change(Sender: TObject);
+begin
+ if cbb1.ItemIndex = 0 then // assuming "YES" is the first item in the combobox
+    lbl8.Caption := '10%' // set the label caption to "10%"
+  else
+    lbl8.Caption := '5%' // set the label caption to "5%"
+end;
+
+procedure TForm1.btn3Click(Sender: TObject);
+//code untuk button edit
+begin
+  begin
+  try
+    // Validasi NIK
+    if Trim(Edit1.Text) = '' then
+    begin
+      ShowMessage('NIK tidak boleh kosong.');
+      Exit;
+    end;
+
+    // Validasi Nama
+    if Trim(Edit2.Text) = '' then
+    begin
+      ShowMessage('Nama tidak boleh kosong.');
+      Exit;
+    end;
+
+    // Menyiapkan perintah SQL untuk memperbarui data customer berdasarkan NIK
+    Form1.ZQuery1.SQL.Clear;
+    Form1.ZQuery1.SQL.Add('UPDATE kustomer SET NAMA = :nama, TELP = :telp, ' +
+                          'EMAIL = :email, ALAMAT = :alamat WHERE NIK = :nik');
+
+    // Menetapkan nilai parameter
+    Form1.ZQuery1.Params.ParamByName('nama').AsString := Edit2.Text;    // Nama baru
+    Form1.ZQuery1.Params.ParamByName('telp').AsString := Edit3.Text;    // Telp baru
+    Form1.ZQuery1.Params.ParamByName('email').AsString := Edit4.Text;  // Email baru
+    Form1.ZQuery1.Params.ParamByName('alamat').AsString := Edit5.Text;  // Alamat baru
+    Form1.ZQuery1.Params.ParamByName('nik').AsString := Edit1.Text;      // NIK dari baris yang akan diperbarui
+
+    // Mengeksekusi perintah SQL
+    Form1.ZQuery1.ExecSQL;
+
+    // Menyusun perintah SQL untuk membuka data yang telah dimasukkan
+    ZQuery1.SQL.Clear;
+    ZQuery1.SQL.Add('SELECT * FROM kustomer');
+    ZQuery1.Open;
+
+    // Refresh DBGrid
+    DBGrid1.DataSource.DataSet := ZQuery1;
+    DBGrid1.Refresh;
+
+    // Menampilkan pesan sukses
+    ShowMessage('Data customer berhasil diupdate untuk NIK yang dimasukkan!');
+  except
+    on E: Exception do
+      ShowMessage('Terjadi kesalahan: ' + E.Message);
+  end;
+end;
+  posisiawal;
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+posisiawal;
+end;
+
+end.
+
+
